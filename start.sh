fecb2f61bcf1ea5d7864f67367b6f555cef546c9
deremiki470-lang <deremiki470@gmail.com>
2025-10-23 11:00:14 -0700

    Fix script syntax and update comments

--- start.sh
+++ start.sh
@@ -2,16 +2,16 @@
 set -e
 
 # Update and install system packages
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends \
+apt-get update
+apt-get install -y --no-install-recommends \
     x11vnc xvfb xfce4 xfce4-goodies pulseaudio pulseaudio-utils \
     websockify novnc python3-pip git
 
-# Make sure noVNC is available
+# Ensure noVNC is available
 if [ ! -d "/usr/share/novnc" ]; then
   echo "Downloading noVNC..."
-  sudo git clone https://github.com/novnc/noVNC.git /usr/share/novnc
-  sudo git clone https://github.com/novnc/websockify.git /usr/share/novnc/utils/websockify || true
+  git clone https://github.com/novnc/noVNC.git /usr/share/novnc
+  git clone https://github.com/novnc/websockify.git /usr/share/novnc/utils/websockify || true
 fi
 
 # Install Python packages
